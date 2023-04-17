d66b58b0d8e48b81e94282851b48e3e14b654376
victor.cruzdefaria@gmail.com <victorcruzdefaria@gmail.com>
2023-04-17 14:00:24 +1000

    fix csv file processing

--- src/data/loader.py
+++ src/data/loader.py
@@ -2,10 +2,15 @@ import pandas as pd
 import os
 
 def merge_data_from_files(path0: str, path1: str, path2: str): 
+
+    if path1:
+        all_data = pd.read_csv(path1, header=0,
+                    usecols=['Date', 'Amount', "Transaction Type", 'Transaction Details', 'Category', 'Merchant Name'],
+                    parse_dates=['Date'])
     # List of file paths to be imported
 
     list_files_paths = [os.path.join(path0, filename) for filename in os.listdir(path0) if filename.endswith('.csv')]
-    list_of_df = []
+    list_of_df = [all_data]
 
     #Import all the files and 
     if len(list_files_paths) > 0:
@@ -28,15 +33,13 @@ def merge_data_from_files(path0: str, path1: str, path2: str):
                 os.remove(file_path)
 
         # Save data into path1
-        df_final = pd.concat(list_of_df, ignore_index=True, axis=0).sort_values(by=['Date'], ascending=True).drop_duplicates()
-        df_final.to_csv(path1, mode='a', index=False, header=False)
-        df_final.to_csv(path2, mode='a', index=False, header=False)
+    df_final = pd.concat(list_of_df, ignore_index=True, axis=0).sort_values(by=['Date'], ascending=True).drop_duplicates()
+    df_final.to_csv(path1, mode='w', index=False)
+    df_final.to_csv(path2, mode='w', index=False)
 
     #Load the expenses data after merging and cleaning to return as Dataframe from path1
-    df_all = pd.read_csv(path1, header=0,
-                parse_dates=['Date']) 
 
 
-    return df_all
+    return df_final
     
 
